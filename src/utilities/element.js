ee52070a72592dda0b089b945931d563ff2ff5d2
Jordan Hurt <jth0024@gmail.com>
2016-04-07 16:55:53 -0500

    Update element.js
    added element prototype

--- src/utilities/element.js
+++ src/utilities/element.js
@@ -1,23 +1,25 @@
 import $ from 'jquery';
-import { assign, reduce } from 'lodash';
+import { merge, reduce } from 'lodash';
 
-export function createElement(Type, config = null, ...children) {
-  const props = assign({}, config, { children });
-  if (typeof Type === 'function') {
-    const componentInstance = new Type(props);
-    return assign(componentInstance.render(), Type.prototype);
-  }
-  return assign({}, {
-    props,
-    toNode() {
-      return $(`<${Type}${buildPropsString(config)}></${Type}>`);
-    },
-  });
+function Element(tagName, attrs, props) {
+  this.props = props;
+  this.node = $(`<${tagName}${buildAttributes(attrs)}></${tagName}>`);
+  this.tagName = tagName;
 }
 
-function buildPropsString(props) {
-  if (props === null) return '';
-  return reduce(props, (result, value, key) => {
+export function createElement(Type, config = null, ...children) {
+  const props = merge({}, config, { children });
+  if (typeof Type === 'function') {
+    const componentInstance = new Type(props);
+    componentInstance.beforeRender();
+    return merge(componentInstance.render(), componentInstance);
+  }
+  return new Element(Type, config, props);
+}
+
+function buildAttributes(config) {
+  if (config === null) return '';
+  return reduce(config, (result, value, key) => {
     return `${result} ${key}="${value}"`;
   }, ' ');
 }
